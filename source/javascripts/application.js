8a2caee8eefaa79fec21238ae246d01f096822bd
Louise Darche <louise.darche@gmail.com>
2018-06-27 16:58:53 +0200

    first version

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+// function dropdownMenu() {
+//   const menu = document.querySelector(".navbar-wagon-item");
+//   console.log(menu);
+//   // menu.addEventListener("click", (event) => {
+//   //   console.log(event);
+//   //   // event.currentTarget.classList.add("menu-open");
+//   // })
+// }
+
+// dropdownMenu()
