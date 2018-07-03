d07813e2e53c4953896ef3a89161c1e925a0a37d
Louise Darche <louise.darche@gmail.com>
2018-07-03 15:57:21 +0200

    ready to show

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,42 +2,27 @@
 //= require bootstrap
 //= require_tree .
 
-// function dropdownMenu() {
-//   const menu = document.querySelector(".navbar-wagon-item");
-//   console.log(menu);
-//   // menu.addEventListener("click", (event) => {
-//   //   console.log(event);
-//   //   // event.currentTarget.classList.add("menu-open");
-//   // })
-// }
 
-// dropdownMenu()
+let imageId = 0;
+function changeWrapper() {
+  if(!imageId){
+    document.querySelector(".right-image").style.backgroundImage = "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.1) 100%), url(images/fond-1-full.jpg";
+    imageId++;
+  }
+  else if (imageId == 1){
+    document.querySelector(".right-image").style.backgroundImage = "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.1) 100%), url(images/fond-2.jpg";
+    imageId++;
+  }
+  else if(imageId == 2){
+    document.querySelector(".right-image").style.backgroundImage = "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.1) 100%), url(images/fond-3.jpg";
+    imageId = 0;
+  }
+}
+window.setInterval(changeWrapper, 5000);
 
-  // const image = document.getElementById("instagram-image");
-  // const url = "https://scontent-bru2-1.cdninstagram.com/vp/3cfc05bb0d6772a1c4f829e2d8aaa4e9/5BDD860D/t51.2885-15/s640x640/sh0.08/e35/34649701_267755117119774_8669603121489510400_n.jpg";
-  // fetch(url)
-  //   .then( data => {
-  //     image.setAttribute('src', data.url);
-  //   })
 
-// let imageId = 0;
-// function changeWrapper(every_seconds) {
-//   const wrapper = document.querySelector(".wrapper");
-//   if(!imageId){
-//     wrapper.style.backgroundImage = "url(images/fond-1.jpg";
-//     imageId++;
-//   }
-//   else if (imageId == 1){
-//     wrapper.style.backgroundImage = "url(images/fond-2.jpg";
-//     imageId++;
-//   }
-//   else if(imageId == 2){
-//     wrapper.style.backgroundImage = "url(images/fond-3.jpg";
-//     imageId = 0;
-//   }
-// }
 
-// setTimeout(changeWrapper(2), 2*1000);
+
 
 // document.addEventListener("DOMContentLoaded", function(event) {
 // })
