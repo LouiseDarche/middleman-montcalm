06c0dbe83cc536a86b76aaac03bb2cafd4393802
Louise Darche <louise.darche@gmail.com>
2018-07-03 18:16:56 +0200

    mobile friendly

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,27 +3,5 @@
 //= require_tree .
 
 
-let imageId = 0;
-function changeWrapper() {
-  if(!imageId){
-    document.querySelector(".right-image").style.backgroundImage = "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.1) 100%), url(images/fond-1-full.jpg";
-    imageId++;
-  }
-  else if (imageId == 1){
-    document.querySelector(".right-image").style.backgroundImage = "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.1) 100%), url(images/fond-2.jpg";
-    imageId++;
-  }
-  else if(imageId == 2){
-    document.querySelector(".right-image").style.backgroundImage = "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.1) 100%), url(images/fond-3.jpg";
-    imageId = 0;
-  }
-}
-window.setInterval(changeWrapper, 5000);
 
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-// })
 
